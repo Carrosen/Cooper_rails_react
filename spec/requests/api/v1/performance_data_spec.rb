2975ccff9ebd124e40237b138e9b580b87c5ed99
zanenkn <zane.neikena@gmail.com>
2019-05-15 13:35:07 +0200

    Adds a sad path request test for authenticating user before showing the performance data, adds another request spec to test if the shown data belongs to the current user.

--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -2,6 +2,7 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
   let(:user) { FactoryBot.create(:user) }
   let(:credentials) { user.create_new_auth_token }
   let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
+  let(:not_headers) { {HTTP_ACCEPT: 'application/json'} }
 
   describe 'POST /api/v1/performance_data' do
     it 'creates a data entry' do
@@ -21,8 +22,20 @@ RSpec.describe Api::V1::PerformanceDataController, type: :request do
 
     it 'returns the collection of performance data' do
       get '/api/v1/performance_data', headers: headers
+      binding.pry
       expect(response_json['entries'].count).to eq 5
     end
+
+    it 'does not return a collection of performance data if user is not logged in' do
+      get '/api/v1/performance_data', headers: not_headers
+      expect(response_json['errors']).to eq ["You need to sign in or sign up before continuing."]
+    end
+
+    it 'returns a collection of performance data that belongs to the logged in user' do
+      get '/api/v1/performance_data', headers: headers
+      expect((response_json['entries']).first['user_id']).to eq user['id']
+    end
+
   end
 
 end
